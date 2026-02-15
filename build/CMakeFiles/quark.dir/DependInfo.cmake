9561210b090c89a060c145572297cdda5f7c3747
grafmorkov <grafmorkov@gmail.com>
2026-02-15 15:18:00 +0300

    Add lexer

---
+++ build/CMakeFiles/quark.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/data/Languages/C_Projects/Quark/src/lexer.c" "CMakeFiles/quark.dir/src/lexer.c.o" "gcc" "CMakeFiles/quark.dir/src/lexer.c.o.d"
+  "/mnt/data/Languages/C_Projects/Quark/src/main.c" "CMakeFiles/quark.dir/src/main.c.o" "gcc" "CMakeFiles/quark.dir/src/main.c.o.d"
+  "/mnt/data/Languages/C_Projects/Quark/src/string.c" "CMakeFiles/quark.dir/src/string.c.o" "gcc" "CMakeFiles/quark.dir/src/string.c.o.d"
+  "/mnt/data/Languages/C_Projects/Quark/src/wordlist.c" "CMakeFiles/quark.dir/src/wordlist.c.o" "gcc" "CMakeFiles/quark.dir/src/wordlist.c.o.d"
+  "" "quark" "gcc" "CMakeFiles/quark.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
